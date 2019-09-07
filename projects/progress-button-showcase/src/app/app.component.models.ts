e70a69c91898ef4672c63aa0fe65d7be4f72877b
Youdede <francesco@youdede.eu>
2019-09-07 20:05:37 +0200

    Improve Responsivity and Description load

---
+++ projects/progress-button-showcase/src/app/app.component.models.ts
@@ -0,0 +1,41 @@
+import {MarkedOptions, MarkedRenderer} from 'ngx-markdown';
+
+
+export function markedOptionsFactory(): MarkedOptions {
+  const renderer = new MarkedRenderer();
+  let headers = [];
+  let cellIndex = 0;
+
+  renderer.table = (header: string, body: string) => {
+    headers = [];
+    return '<div class="table"><div class="header">' + header + '</div><div class="table-body">' + body + '</div></div>';
+  };
+
+  renderer.tablerow = (content: string) => {
+    cellIndex = 0;
+    return '<div class="row">' + content + '</div>';
+  };
+
+  renderer.tablecell = (content: string, flags) => {
+    let title = null;
+    if (flags.header) {
+      headers.push(content);
+    } else {
+      title = headers[cellIndex];
+      cellIndex++;
+    }
+    return '<div class="cell" ' + ((title !== null) ? 'data-title="' + title + '"' : '') + '>' + content.replace(/,/g, '') + '</div>';
+  };
+
+  return {
+    renderer,
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: false,
+    smartLists: true,
+    smartypants: false,
+  };
+}
+
--- projects/progress-button-showcase/src/app/app.module.ts
+++ projects/progress-button-showcase/src/app/app.module.ts
@@ -1,10 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {ProgressButtonModule} from '../../../progress-button/src/lib/progress-button.module';
 import {NgSelectModule} from '@ng-select/ng-select';
+import {MarkdownModule, MarkedOptions} from 'ngx-markdown';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {markedOptionsFactory} from './app.component.models';
 
 @NgModule({
   declarations: [
@@ -16,8 +19,17 @@ import {NgSelectModule} from '@ng-select/ng-select';
     NgSelectModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
+    MarkdownModule.forRoot({
+      loader: HttpClient,
+      markedOptions: {
+        provide: MarkedOptions,
+        useFactory: markedOptionsFactory,
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
