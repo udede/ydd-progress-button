5d981d30c6cabcaf316a98eaf5afb3e9fa419f99
Youdede <francesco@youdede.eu>
2019-09-09 01:18:35 +0200

    test: Add 'lcovonly' to Karma config

--- projects/progress-button-showcase/karma.conf.js
+++ projects/progress-button-showcase/karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../../coverage/progress-button-showcase'),
-      reports: ['html', 'text-summary'],
+      reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
--- projects/progress-button/karma.conf.js
+++ projects/progress-button/karma.conf.js
@@ -13,11 +13,11 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../../coverage/progress-button'),
-      reports: ['html',  'text-summary'],
+      reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
