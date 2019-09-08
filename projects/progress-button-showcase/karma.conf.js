fb917036c721b51ef4b8fe5d31cba0f9ad4f37c1
Youdede <francesco@youdede.eu>
2019-09-09 00:51:16 +0200

    test: Add coverage folder to Git

--- projects/progress-button-showcase/karma.conf.js
+++ projects/progress-button-showcase/karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../../coverage/progress-button-showcase'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      reports: ['html', 'text-summary'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
